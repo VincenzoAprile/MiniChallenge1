ae34da9f73cdff3bcad51ff4467dc9731367f287
Vincenzo Aprile <aprile.vincenzo202@gmail.com>
2023-11-19 19:06:39 +0100

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniChallenge1
+//
+//  Created by Vincenzo Aprile on 19/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge1App.swift
+//  MiniChallenge1
+//
+//  Created by Vincenzo Aprile on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
